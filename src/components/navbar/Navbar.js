12c4f471668e79ea924c70a91c9bcea530f88b69
VarunVaidhyanathan <79815744+VarunVaidhyanathan@users.noreply.github.com>
2021-12-30 16:19:36 -0800

    Basic navbar made without page redirection

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+            <nav className="navbar navbar-static-top" style={{
+                backgroundColor: "#fe2c54"
+            }}>
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#" style={{color:"#FBFAF5", fontWeight:"bold"}}>MyPoke</a>
+                    </div>
+                    <div class="pull-right">
+                    <ul className="nav navbar-nav">
+                        <li className="active"><a href="#" style={{color:"#FBFAF5", fontWeight:"bold"}}>Home</a></li>
+                        <li><a href="#" style={{color:"#FBFAF5", fontWeight:"bold" }}>Your favorite Pokemon</a></li>
+                    </ul>
+                    </div>
+               </div>
+            </nav>
+            </div>
+        )
+    }
+}
